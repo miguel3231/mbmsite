95555c4260e737d51ca287dfb7b3145a72a494b6
Miguel Beltrán <miguel.beltran@sparktechs.com>
2021-06-16 15:28:37 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Section from './Section.js';
+import './bootstrap.min.css'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Mike's Site
+        <div class="container">
+          <Section text={"Text1"} />
+          <Section text={"Text2"} />
+        </div>
       </header>
     </div>
   );
---
+++ src/Section.js
@@ -0,0 +1,9 @@
+const Section = props => {
+    return (
+        <div class="row">
+            {props.text}
+        </div>
+    )
+}
+
+export default Section;
