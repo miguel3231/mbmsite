be888e6bb9a2cbc3b1c391eba56edc3b82577d73
Miguel Beltrán <miguel.socialmedia@outlook.com>
2021-08-07 15:09:26 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Section from './Section.js';
-import './bootstrap.min.css'
+import Navbar from './components/Navbar.js';
+import "bootstrap/dist/css/bootstrap.min.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Mike's Site
-        <div class="container">
-          <Section text={"Test change"} />
-          <Section text={"Test change 2"} />
-        </div>
-      </header>
-    </div>
+    <Navbar />
   );
 }
 
--- src/Section.js
+++
@@ -1,9 +0,0 @@
-const Section = props => {
-    return (
-        <div class="row">
-            {props.text}
-        </div>
-    )
-}
-
-export default Section;
